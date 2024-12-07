4248bb22d1bd0050db22d68d8de6720d6d345ea9
Carey Fang <carey020281@gmail.com>
2024-12-07 22:56:04 +0800

    bug

--- manage.py
+++ manage.py
@@ -8,11 +8,6 @@ def main():
     # Set the Flask app environment variable
     os.environ['FLASK_APP'] = 'app.py'  # Replace 'app.py' if different
 
-    # Retrieve the secret key from environment variables
-    initialize_secret = os.environ.get('INITIALIZE_SECRET')
-    if not initialize_secret:
-        print("Error: INITIALIZE_SECRET environment variable not set.")
-        sys.exit(1)
 
     # Define the URL for initialization
     initialize_url = 'https://careyfang.pythonanywhere.com/initialize_data'  # Replace 'careyfang'
@@ -21,7 +16,6 @@ def main():
     try:
         response = requests.post(
             initialize_url,
-            data={'secret_key': initialize_secret}
         )
         if response.status_code == 200:
             print("Initialization successful.")
